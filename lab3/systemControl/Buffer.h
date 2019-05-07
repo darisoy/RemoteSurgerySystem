0aa0943e2b6f126b093393ae72423a457e3e5be4
rrwang97 <49732209+rrwang97@users.noreply.github.com>
2019-05-06 17:54:13 -0700

    Update Buffer.h

--- lab3/systemControl/Buffer.h
+++ lab3/systemControl/Buffer.h
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
-void BufferFunction(BufferData * buff, char * bufferPtr, int size);
+void BufferFunction(BufferData * buff, double * bufferPtr, int size);
 
-void BufferWrite(Buffer * buff, (void*) data);
+void BufferWrite(Buffer * buff, double data);
 
 double BufferRead(Buffer * buff);

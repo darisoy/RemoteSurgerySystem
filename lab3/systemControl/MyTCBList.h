fdeeabeeeb1a7d0a16dc2d87a46e3988f05f9395
rrwang97 <49732209+rrwang97@users.noreply.github.com>
2019-05-07 17:44:05 -0700

    Create MyTCBList.h

---
+++ lab3/systemControl/MyTCBList.h
@@ -0,0 +1,5 @@
+void schedulerFunction(MyTCBList * TCBListPtr, MyTCB * frontTCB);
+
+void schedulerInsert(MyTCBList * TCBListPtr, MyTCB * ITCB, int location);
+
+void schedulerDelete(MyTCBList * TCBListPtr, int location);

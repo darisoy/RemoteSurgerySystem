1aaa76e9611601b679ec2e69c2de0ad2363918a3
Doruk Arisoy <doruk.arisoy@gmail.com>
2019-05-09 11:50:44 -0700

    uno is back

---
+++ lab3/peripheralSubsystem/dataStructs.h
@@ -0,0 +1,31 @@
+//Peripheral Subsystem Variables
+#ifndef dataStructs                     //check to see if variables are defined elsewhere
+#define dataStructs                     //assigns definiton to dataStructs
+#include <stdbool.h>                    //import necessary files
+
+enum _myBool { FALSE = 0, TRUE = 1 };   //define boolean values
+typedef enum _myBool Bool;              //creates the boolean variable
+
+unsigned int temperatureRaw;            //initializes temperature variable
+unsigned int systolicPressRaw;          //initializes syst. press. variable
+unsigned int diastolicPressRaw;         //initializes dias. press. variable
+unsigned int pulseRateRaw;              //initializes pulse rate variable
+
+boolean pinHigh;
+
+int tempBool = 0;                       //initialize temperature boolean to be 0
+int pulseBool = 0;                      //initialize pulse boolean to be 0
+int* pTempCount;                        //initialize the pointer to the temp counter
+int* pPulseCount;                       //initialize the pointer to the pulse counter
+int* pSysCount;                         //initialize the pointer to the systolic counter
+int* pDiaCount;                         //initialize the pointer to the diastolic counter
+int* pDiastolicFunction;                //initialize the pointer to the diastolic function
+int* pSystolicFunction;                 //initialize the pointer to the systolic function
+
+struct controlMeasureData {             //create the MeasureData struct
+    unsigned int* pTemperatureRaw;      //struct contains temp data
+    unsigned int* pSystolicPressRaw;    //struct contains syst. press. data
+    unsigned int* pDiastolicPressRaw;   //struct contains dia. press. data
+    unsigned int* pPulseRateRaw;        //struct contains pulse rate data
+} MeasureData;                          //struct name
+#endif
---
+++ lab3/peripheralSubsystem/peripheralSubsystem.ino
@@ -0,0 +1,30 @@
+//Peripheral Subsystem
+#include "dataStructs.h"            //connect variables from dataStructs file
+#include "Measure.h"                // connect the function to the header file
+#include <FreqMeasure.h>
+
+void setup() {                      //setup portion of the arduino code
+    initialize();                   //call the initialize function
+    Serial.begin(9600);             //initialize the serial with 9600 baud rate
+    pinMode(13, INPUT);             //setup pin 13 to be an input
+    pinMode(12, INPUT);             //setup pin 12 to be an input
+    pinMode(11, INPUT);             //setup pin 11 to be an input
+    pinMode(10, INPUT);             //setup pin 10 to be an input
+    FreqMeasure.begin();
+}
+
+void loop() {                       //code arduino constatly loops through
+    measureFunction(MeasureData,    //pass in struct
+                    pTempCount,     //pass in the pointer that points to temp count
+                    pPulseCount,    //pass in the pointer that points to pulse count
+                    pSysCount,      //pass in the pointer that points to systolic count
+                    pDiaCount);     //pass in the pointer that points to diastolic count
+}
+
+void initialize() {                 //initializing the global variables
+    temperatureRaw    = 75;         //set initial temperature to be 75
+    systolicPressRaw  = 80;         //set initial systolic pressure to be 80
+    diastolicPressRaw = 80;         //set initial diastolic pressure to be 80
+    pulseRateRaw      = 50;         //set initial pulse rate to be 50
+    pinHigh = false;
+}

76e64b227ff50b56a6f3e338449962607bf32281
kstefanovicz <49795722+kstefanovicz@users.noreply.github.com>
2019-05-12 18:04:32 -0700

    read last instead of first

--- lab3/systemControl/Compute.ino
+++ lab3/systemControl/Compute.ino
@@ -15,8 +15,8 @@ void computeFunction(void* computeDataStruct){                                  
     cData->pDiastolicPressCorrected   = &diastolicPressCorrected;                      //assign corrected dia's address to corrected dia pointer from compute struct
     cData->pPulseRateCorrected        = &pulseRateCorrected;                           //assign corrected pulse's address to corrected pulse pointer from compute struct
 
-    tempComputedData.push(5 + (0.75 * (tempRawData.first())));         //convert raw temp and point pTempComputed pointer to it
-    sysComputedData.push(9 + (2    * (sysRawData.first())));       //convert raw sys and point pSystolicPressComputed pointer to it
-    diaComputedData.push(6 + (1.5  * (diaRawData.first())));      //convert raw dia and point pDiastolicPressComputed pointer to it
-    pulseComputedData.push(8 + (3    * (pulseRawData.first())));           //convert raw pulse and point pPulseRateComputed pointer to it
+    tempComputedData.push(5 + (0.75 * (tempRawData.last())));         //convert raw temp and point pTempComputed pointer to it
+    sysComputedData.push(9 + (2    * (sysRawData.last())));       //convert raw sys and point pSystolicPressComputed pointer to it
+    diaComputedData.push(6 + (1.5  * (diaRawData.last())));      //convert raw dia and point pDiastolicPressComputed pointer to it
+    pulseComputedData.push(8 + (3    * (pulseRawData.last())));           //convert raw pulse and point pPulseRateComputed pointer to it
 }

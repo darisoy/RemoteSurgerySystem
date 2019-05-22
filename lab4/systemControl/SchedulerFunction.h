fb9c971012164e8b0db9af40a18c8fb664a146ad
Richard Wang <rrwang@uw.edu>
2019-05-22 12:49:28 -0700

    fixed scheduler

--- lab4/systemControl/SchedulerFunction.h
+++ lab4/systemControl/SchedulerFunction.h
@@ -1,6 +1,7 @@
-void schedulerFunctionAdd(MyTCB* TCBPter, void* LinkedList);        //header for schedulerFunctionAdd
+void schedulerFunctionAdd(MyTCB* TCBPtr, void* LinkedList);        //header for schedulerFunctionAdd
                                                                     //takes in the pointer for task and for LinkedList
 void schedulerFunctionDelete(MyTCB* TCBPtr, void* LinkedList);      //header for schedulerFunctionDelete
                                                                     //takes in the pointer for task and for LinkedList
 void schedulerFunctionRun(void* LinkedList);                        //header for schedulerFunctionRun
                                                                     //takes in the pointer for LinkedList
+void schedulerContains(MyTCB* TCBPtr, void* LinkedList);
